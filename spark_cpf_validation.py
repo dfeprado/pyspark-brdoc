076d4191e433e7db91cf872bd5e2dd2b9ad67b51
Daniel Prado <ferrpra@terceirizadojsafra.com.br>
2024-05-09 13:22:32 -0300

    Melhora o código

--- spark_cpf_validation.py
+++ spark_cpf_validation.py
@@ -33,91 +33,89 @@ class CPFValidator:
         self.__outputColName = outputColName
 
     def validate(self, df: DataFrame) -> DataFrame:
-        invalidRowsDf = df.filter(
+        invalidDocsDf = df.filter(
             (df[self.__docColName].isin(CPFValidator._KNOWN_INVALID_DOCS))
             | (~df[self.__docColName].rlike(r"^[0-9]{11}$"))
-        )
-
-        workingDf = df.exceptAll(invalidRowsDf)
-
-        # Quebra o CPF em 3 partes:
-        #   1. CPF completo
-        #   2. Dígitos de base (1-8), em um array
-        #   3. Dígitos verificadores.
-        df2 = workingDf.select(
-            self.__docColName,
-            sqlfn.split(df[self.__docColName].substr(1, 9), "", 9).alias(
-                CPFValidator.__Col.BASE_DIGIT
-            ),
-            df[self.__docColName]
-            .substr(10, 12)
-            .alias(CPFValidator.__Col.VERIFICATION_DIGITS),
-        )
-
-        # Explode os dígitos de base para cada um na sua linha
-        df3 = df2.select(
-            self.__docColName,
-            sqlfn.explode(CPFValidator.__Col.BASE_DIGIT).alias(
-                CPFValidator.__Col.BASE_DIGIT
-            ),
-            CPFValidator.__Col.VERIFICATION_DIGITS,
-        ).withColumn(
-            CPFValidator.__Col.BASE_DIGIT,
-            sqlfn.col(CPFValidator.__Col.BASE_DIGIT).cast("int"),
-        )
-
-        # Cria as colunas de peso para cálculo dos dígitos 1 e 2
-        df5 = df3.withColumn(
-            CPFValidator.__Col.DIG_1_WEIGTH,
-            sqlfn.row_number().over(
-                Window().partitionBy(self.__docColName).orderBy(self.__docColName)
-            ),
-        ).withColumn(
-            CPFValidator.__Col.DIG_2_WEIGTH,
-            sqlfn.col(CPFValidator.__Col.DIG_1_WEIGTH) - 1,
-        )
-
-        # Gera o produto dos dígitos de base por seus pesos, para ambos os dígitos
-        # verificadores
-        df6 = df5.withColumn(
-            CPFValidator.__Col.PRODUCT_DIG_1,
-            df5[CPFValidator.__Col.BASE_DIGIT] * df5[CPFValidator.__Col.DIG_1_WEIGTH],
-        ).withColumn(
-            CPFValidator.__Col.PRODUCT_DIG_2,
-            df5[CPFValidator.__Col.BASE_DIGIT] * df5[CPFValidator.__Col.DIG_2_WEIGTH],
-        )
-        df6.show()
-
-        # Soma os produtos
-        df7 = df6.groupBy(
-            self.__docColName, CPFValidator.__Col.VERIFICATION_DIGITS
-        ).agg(
-            sqlfn.sum(CPFValidator.__Col.PRODUCT_DIG_1).alias(
-                CPFValidator.__Col.SUM_PRODUCT_DIG_1
-            ),
-            sqlfn.sum(CPFValidator.__Col.PRODUCT_DIG_2).alias(
-                CPFValidator.__Col.SUM_PRODUCT_DIG_2
-            ),
         )
 
         def fixComputedDigit(colName: str) -> Column:
             col = sqlfn.col(colName)
             return sqlfn.when(col != 10, col).otherwise(0)
 
-        # Gera os dígitos verificadores
-        df8 = (
-            df7.withColumn(
+        computedDigverDf = (
+            # Quebra o CPF em 3 partes:
+            #   1. CPF completo
+            #   2. Raíz (dígitos de índices 1-8), em um array
+            #   3. Dígitos verificadores (digver; dígitos de índices 10-12).
+            #
+            # Cada dígito da raíz estará na sua própria linha.
+            df.exceptAll(invalidDocsDf)
+            .select(
+                self.__docColName,
+                sqlfn.explode(
+                    sqlfn.split(df[self.__docColName].substr(1, 9), "", 9)
+                ).alias(CPFValidator.__Col.BASE_DIGIT),
+                df[self.__docColName]
+                .substr(10, 12)
+                .alias(CPFValidator.__Col.VERIFICATION_DIGITS),
+            )
+            # Converte cada dígito da raiz de string para int
+            .withColumn(
+                CPFValidator.__Col.BASE_DIGIT,
+                sqlfn.col(CPFValidator.__Col.BASE_DIGIT).cast("int"),
+            )
+            # Cria a coluna de peso para o digver 1 (números de 1-9)
+            .withColumn(
+                CPFValidator.__Col.DIG_1_WEIGTH,
+                sqlfn.row_number().over(
+                    Window().partitionBy(self.__docColName).orderBy(self.__docColName)
+                ),
+            )
+            # Cria as colunas de peso para o digver 2 (números de 0-8)
+            # Eu sei, eu sei... o correto é de 0-9. Mas como ainda não temos o
+            # primeiro dígito verificador calculado, eu vou só até o penúltimo.
+            .withColumn(
+                CPFValidator.__Col.DIG_2_WEIGTH,
+                sqlfn.col(CPFValidator.__Col.DIG_1_WEIGTH) - 1,
+            )
+            # Calcula o produto da raíz pelo peso do digver 1
+            .withColumn(
+                CPFValidator.__Col.PRODUCT_DIG_1,
+                sqlfn.col(CPFValidator.__Col.BASE_DIGIT)
+                * sqlfn.col(CPFValidator.__Col.DIG_1_WEIGTH),
+            )
+            # Calcula o produto da raíz pelo peso do digver 2
+            .withColumn(
+                CPFValidator.__Col.PRODUCT_DIG_2,
+                sqlfn.col(CPFValidator.__Col.BASE_DIGIT)
+                * sqlfn.col(CPFValidator.__Col.DIG_2_WEIGTH),
+            )
+            # Soma os produtos das somas, cada um em sua coluna.
+            .groupBy(self.__docColName, CPFValidator.__Col.VERIFICATION_DIGITS)
+            .agg(
+                sqlfn.sum(CPFValidator.__Col.PRODUCT_DIG_1).alias(
+                    CPFValidator.__Col.SUM_PRODUCT_DIG_1
+                ),
+                sqlfn.sum(CPFValidator.__Col.PRODUCT_DIG_2).alias(
+                    CPFValidator.__Col.SUM_PRODUCT_DIG_2
+                ),
+            )
+            # Calcula o digver 1
+            .withColumn(
                 CPFValidator.__Col.COMPUTED_DIG_1,
-                df7[CPFValidator.__Col.SUM_PRODUCT_DIG_1] % 11,
+                sqlfn.col(CPFValidator.__Col.SUM_PRODUCT_DIG_1) % 11,
             )
             .withColumn(
                 CPFValidator.__Col.COMPUTED_DIG_1,
                 fixComputedDigit(CPFValidator.__Col.COMPUTED_DIG_1),
             )
+            # Calcula o digver 2
+            # Veja que agora eu adiciono o nono peso já que tenho o digver 1
+            # agora.
             .withColumn(
                 CPFValidator.__Col.COMPUTED_DIG_2,
                 (
-                    df7[CPFValidator.__Col.SUM_PRODUCT_DIG_2]
+                    sqlfn.col(CPFValidator.__Col.SUM_PRODUCT_DIG_2)
                     + 9 * sqlfn.col(CPFValidator.__Col.COMPUTED_DIG_1)
                 )
                 % 11,
@@ -126,24 +124,21 @@ class CPFValidator:
                 CPFValidator.__Col.COMPUTED_DIG_2,
                 fixComputedDigit(CPFValidator.__Col.COMPUTED_DIG_2),
             )
+            # Concatena os dígitos verificadores obtidos.
+            .withColumn(
+                CPFValidator.__Col.COMPUTED_VERIFICATION_DIGITS,
+                sqlfn.concat(
+                    CPFValidator.__Col.COMPUTED_DIG_1,
+                    CPFValidator.__Col.COMPUTED_DIG_2,
+                ),
+            )
         )
-        df8.show()
-
-        # concatena os dígitos verificadores
-        df9 = df8.withColumn(
-            CPFValidator.__Col.COMPUTED_VERIFICATION_DIGITS,
-            sqlfn.concat(
-                CPFValidator.__Col.COMPUTED_DIG_1,
-                CPFValidator.__Col.COMPUTED_DIG_2,
-            ),
-        )
-        df9.show()
 
         # Retorna os documentos com o status de validação.
-        return df9.select(
+        return computedDigverDf.select(
             self.__docColName,
             (
-                df9[CPFValidator.__Col.VERIFICATION_DIGITS]
-                == df9[CPFValidator.__Col.COMPUTED_VERIFICATION_DIGITS]
+                computedDigverDf[CPFValidator.__Col.VERIFICATION_DIGITS]
+                == computedDigverDf[CPFValidator.__Col.COMPUTED_VERIFICATION_DIGITS]
             ).alias(self.__outputColName),
-        ).union(invalidRowsDf.select(self.__docColName, sqlfn.lit(False)))
+        ).union(invalidDocsDf.select(self.__docColName, sqlfn.lit(False)))
